e71ae6224f823f587dfb965e88d2b3e8845aa6b4
spencerneveux <spencerneveux@gmail.com>
2019-06-24 16:45:02 -0700

    fixing movie

--- movie.py
+++ movie.py
@@ -1,9 +1,11 @@
 import requests
 
+KEY = 'd6982a301b9628f78bb3be565a945c79'
+
 class Movie:
 
 	def __init__(self):
-		self.movie = requests.get('https://api.themoviedb.org/3/movie/now_playing?api_key=d6982a301b9628f78bb3be565a945c79&language=en-US&page=1').json()
+		self.movie = requests.get('https://api.themoviedb.org/3/movie/now_playing?api_key='+ KEY + '&language=en-US&page=1').json()
 
 	def get_data(self):
 		return self.movie['results'] 
@@ -12,26 +14,26 @@ class Movie:
 		return len(self.movie['results'])
 
 	def get_movie_details(self, id):
-		details = requests.get('https://api.themoviedb.org/3/movie/' + id + '?api_key=d6982a301b9628f78bb3be565a945c79&language=en-US').json()
+		details = requests.get('https://api.themoviedb.org/3/movie/' + id + '?api_key='+ KEY + '&language=en-US').json()
 		return details
 
 	def get_tv_details(self, id):
-		details = requests.get('https://api.themoviedb.org/3/tv/' + id + '?api_key=d6982a301b9628f78bb3be565a945c79&language=en-US').json()
+		details = requests.get('https://api.themoviedb.org/3/tv/' + id + '?api_key='+ KEY + '&language=en-US').json()
 		return details
 
 	def get_movie_video(self, id):
-		video = requests.get('https://api.themoviedb.org/3/movie/' + id + '/videos?api_key=d6982a301b9628f78bb3be565a945c79&language=en-US').json()
+		video = requests.get('https://api.themoviedb.org/3/movie/' + id + '/videos?api_key='+ KEY + '&language=en-US').json()
 		return video
 
 	def get_tv_video(self, id):
-		video = requests.get('https://api.themoviedb.org/3/tv/' + id + '/videos?api_key=d6982a301b9628f78bb3be565a945c79&language=en-US').json()
+		video = requests.get('https://api.themoviedb.org/3/tv/' + id + '/videos?api_key='+ KEY + '&language=en-US').json()
 		return video
 
 	def get_trending(self):
-		trending = requests.get('https://api.themoviedb.org/3/trending/all/day?api_key=d6982a301b9628f78bb3be565a945c79').json()
+		trending = requests.get('https://api.themoviedb.org/3/trending/all/day?api_key='+ KEY + '').json()
 		return trending
 
 
 	def get_tv(self):
-		tv = requests.get('https://api.themoviedb.org/3/tv/popular?api_key=d6982a301b9628f78bb3be565a945c79&language=en-US&page=1').json()
+		tv = requests.get('https://api.themoviedb.org/3/tv/popular?api_key='+ KEY + '&language=en-US&page=1').json()
 		return tv
